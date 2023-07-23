c0e855068aae347a31ab7326d65a96bff67502e9
Sandeep Bhanja <sandeepbhanja0718@gmail.com>
2023-07-23 17:03:15 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/',(req,res)=>{
+    res.send('API Running');
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`);
+})
